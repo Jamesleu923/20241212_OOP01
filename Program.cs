f07cd381a7c953a76b9dccd7cdc34dd4da135192
jamesleu923 <jamesleu923@gmail.com>
2024-12-12 10:43:49 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20241212_OOP01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
